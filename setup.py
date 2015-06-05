ed24ba311471a3720576bd412d7779adfe2d926b
dongweiming <ciici1234@hotmail.com>
2015-06-05 22:34:55 +0800

    Add filter fn for end callback

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def fread(fname):
 
 
 setup(name='flask_slackbot',
-      version='0.0.11',
+      version='0.1.0',
       url='https://github.com/python-cn/flask-slackbot',
       license='MIT',
       author='halfcrazy',
