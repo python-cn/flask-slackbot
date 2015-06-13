647ed98c92137b52c865b1d1f5d5c6bd2b7b31f8
dongweiming <ciici1234@hotmail.com>
2015-06-14 06:31:52 +0800

    Add SLACK_CHAT_TOKEN setting

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def fread(fname):
 
 
 setup(name='flask_slackbot',
-      version='0.1.3',
+      version='0.1.4',
       url='https://github.com/python-cn/flask-slackbot',
       license='MIT',
       author='halfcrazy',
